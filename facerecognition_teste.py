3d834975098badb1a0fa43d8b262a7170709097f
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-04-12 16:48:46 -0300

    Update facerecognition_teste.py

--- facerecognition_teste.py
+++ facerecognition_teste.py
@@ -2,12 +2,12 @@ import numpy as np
 import face_recognition as fr
 import cv2
 from read import so_return
-from add_face import get_rostos
 
 rostos_conhecidos, nomes_dos_rostos = so_return()
-# rostos_conhecidos, nomes_dos_rostos = get_rostos()
 video_capture = cv2.VideoCapture(0)
 
+count = 0
+nome_teste = ""
 
 while True:
     ret, frame = video_capture.read()
@@ -22,10 +22,29 @@ while True:
         face_distances = fr.face_distance(rostos_conhecidos, rosto_desconhecido)
 
         melhor_id = np.argmin(face_distances)
+
+        for i in range(len(resultados)):
+            contador = 0
+            if resultados[i] == True:
+                contador += 1
+        
+        if contador > 1:
+            count = 0
+            nome = "Desconhecido"
+
         if resultados[melhor_id]:
             nome = nomes_dos_rostos[melhor_id]
+            if nome == nome_teste:
+                count += 1
+            else:
+                count = 0
+            
+            nome_teste = nome
         else:
+            count = 0
             nome = "Desconhecido"
+
+        
 
         # Ao redor do rosto
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -38,6 +57,12 @@ while True:
         cv2.putText(frame, nome, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         cv2.imshow('Webcam_facerecognition', frame)
+
+        if count == 5:
+            print(nome)
+            count = 0
+        elif count == 0:
+            print(nome)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
